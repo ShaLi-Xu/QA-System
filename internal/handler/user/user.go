a31608dedd6d1c33c0b0c8920f06aa315440638c
hstardawn <2198936039@qq.com>
2025-12-03 17:33:15 +0800

    refactor: 通过引入不同身份态，统一管理员与用户查看问卷的接口

--- internal/handler/user/user.go
+++ internal/handler/user/user.go
@@ -170,7 +170,8 @@ func SubmitSurvey(c *gin.Context) {
 }
 
 type getSurveyData struct {
-	ID int64 `form:"id" binding:"required"`
+	ID           int64 `form:"id" binding:"required"`
+	IsPreVisible int8  `form:"isPreVisible"`
 }
 
 // GetSurvey 用户获取问卷
@@ -179,6 +180,12 @@ func GetSurvey(c *gin.Context) {
 	err := c.ShouldBindQuery(&data)
 	if err != nil {
 		code.AbortWithException(c, code.ParamError, err)
+		return
+	}
+	// 获取用户信息
+	user, err := service.GetUserSession(c)
+	if err != nil && !errors.Is(err, errors.New("")) {
+		code.AbortWithException(c, code.ServerError, err)
 		return
 	}
 	// 获取问卷
@@ -193,7 +200,7 @@ func GetSurvey(c *gin.Context) {
 		return
 	}
 	// 判断问卷是否开放
-	if survey.Status != 2 {
+	if survey.Status != 2 && (user == nil || data.IsPreVisible == 0) {
 		code.AbortWithException(c, code.SurveyNotOpen, errors.New("问卷未开放"))
 		return
 	}
